7e798e9026acacf21a724d5553bc1cac14f2806c
Lisha Ballard <ldom3976@lishas-air.mynetworksettings.com>
2023-05-17 11:45:55 -0400

    updated server.js

--- server.js
+++ server.js
@@ -3,10 +3,28 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 
+//helper function
+
+//add handlebars
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+//session connection
+const session = require('express-session');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
 
 app.use(session(sess));
 app.use(express.json());
@@ -18,3 +36,7 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
   });
+
+  //need to add helpers
+  //need to figure out error in post routes
+  //need to work on login/logout
