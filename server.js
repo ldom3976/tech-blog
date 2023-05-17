68096b38c29c50966198577641f44b1c8d0f73e0
Lisha Ballard <ldom3976@lishas-air.mynetworksettings.com>
2023-05-17 12:45:41 -0400

    started handlebars and helpers

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 
-//helper function
+
+const helpers = require('./utils/helpers');
 
 //add handlebars
+//const hbs = exphbs.create({helpers});
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -30,6 +32,9 @@ app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(routes);
 
