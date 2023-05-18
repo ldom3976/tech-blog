0b9ce343780d107f15b4e9d0dd302fda28721bf3
Lisha Ballard <ldom3976@lishas-air.mynetworksettings.com>
2023-05-18 15:19:18 -0400

    updated/changed server format

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
+const path = require('path');
 const express = require('express');
+const session = require('express-session');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const path = require('path');
-
-
 const helpers = require('./utils/helpers');
-
-//add handlebars
-//const hbs = exphbs.create({helpers});
-
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-//session connection
-const session = require('express-session');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
@@ -29,12 +24,15 @@ const sess = {
 
 
 app.use(session(sess));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+
+const hbs = exphbs.create({helpers});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
@@ -42,6 +40,3 @@ sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
   });
 
-  //need to add helpers
-  //need to figure out error in post routes
-  //need to work on login/logout
