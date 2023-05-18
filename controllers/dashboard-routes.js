933fc547ddc8ba125009610683dfa8e7149b559e
Lisha Ballard <ldom3976@lishas-air.mynetworksettings.com>
2023-05-18 17:15:10 -0400

    changing routes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,23 +1,19 @@
 const router = require('express').Router();
-const sequelize = require('../config/connection');
+//const sequelize = require('../config/connection');
 const {Post, User, Comment} = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', withAuth, (req, res) => {
-    Post.findAll({
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findAll({
       where: {
         user_id: req.session.user_id
       },
-      attributes: [
-        'id',
-        'title',
-        'content',
-        'created_at'
-      ],
+      attributes: [ 'id', 'post_body', 'title', 'created_at'],
       include: [
         {
           model: Comment,
-          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          attributes: ['id', 'comment_body', 'post_id', 'user_id', 'created_at'],
           include: {
             model: User,
             attributes: ['username']
@@ -28,27 +24,34 @@ router.get('/', withAuth, (req, res) => {
           attributes: ['username']
         }
       ]
-    })
-    .then(dbPostData => {
-        const posts = dbPostData.map(post => post.get({ plain: true }));
-        res.render('dashboard', { posts, loggedIn: true });
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+    });
+    const posts = postData.map((post) => post.get({ plain: true }));
 
-  router.get('/edit/:id', withAuth, (req, res) => {
-    Post.findOne({
+    res.render('all-posts-admin', {
+      layout: 'dashboard',
+      posts,
+    });
+  } catch (err) {
+    res.redirect('login');
+  }
+});
+
+router.get('/new', withAuth, (req, res) => {
+  res.render('new-post', {
+    layout: 'dashboard',
+  });
+});
+
+
+router.get('/edit/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findOne({
         where: {
           id: req.params.id
         },
-        attributes: ['id', 
-                     'title',
-                     'content',
-                     'created_at'
-                  ],
+        attributes: [
+          'id', 'title', 'post_body', 'created_at'
+        ],
         include: [
           {
             model: User,
@@ -56,32 +59,28 @@ router.get('/', withAuth, (req, res) => {
           },
           {
             model: Comment,
-            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            attributes: ['id', 'comment_body', 'post_id', 'user_id', 'created_at'],
             include: {
               model: User,
               attributes: ['username']
             }
           }
         ]
-      })
-      .then(dbPostData => {
-        if (!dbPostData) {
-          res.status(404).json({ message: 'No post found.' });
-          return;
-        }
-          const post = dbPostData.get({ plain: true });
-          res.render('edit-post', {post, loggedIn: true});
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
       });
-})
 
-router.get('/new', (req, res) => {
-    res.render('new-post');
-});
-
+      if (postData) {
+        const post = postData.get({ plain: true });
+        res.render('edit-post', {
+          layout: 'dashboard',
+          post,
+        });
+      } else {
+        res.status(404).end();
+      }
+    } catch (err) {
+      res.redirect('login');
+    }
+  });
 
 
 module.exports = router;
